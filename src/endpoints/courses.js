058961cc6f736908666a45b72292ccf79c44d761
illdub <illia.dubrovin@dreamhost.com>
2025-06-10 01:21:55 +0200

    edit

--- src/endpoints/courses.js
+++ src/endpoints/courses.js
@@ -3,7 +3,7 @@ import { SERVICE_ORIGIN } from "../contants";
 
 export async function getCourseById() {
   try {
-    const result = await axios.get(`${SERVICE_ORIGIN}/courses/1`);
+    const result = await axios.get(`${SERVICE_ORIGIN}/courses/stripped/1`);
     return result.data;
   } catch {
     return null;
